de5c0a947edbb4761895764a59c80bbf78044014
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-10-28 20:38:19 +0530

    Create DAY1DATATYPES.py

---
+++ CODINGNINJAS/DAY1DATATYPES.py
@@ -0,0 +1,10 @@
+from typing import *
+
+def dataTypes(type: str):
+    dicti={"Integer":4,"Long":8,"Float":4,"Double":8,"Character":1}
+    return dicti[type]
+    pass
+
+
+
+https://www.codingninjas.com/studio/problems/data-type_9065109?challengeSlug=ninja-slayground&leftPanelTab=0
